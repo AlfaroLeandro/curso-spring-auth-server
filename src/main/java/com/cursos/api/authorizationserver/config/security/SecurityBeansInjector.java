ca10c0bd53d56d03d4157c5f7c8a83c1cad515b4
Leandro <being.leeaa15@gmail.com>
2024-02-04 21:19:44 -0300

    dependencias

--- src/main/java/com/cursos/api/authorizationserver/config/security/SecurityBeansInjector.java
+++ src/main/java/com/cursos/api/authorizationserver/config/security/SecurityBeansInjector.java
@@ -1,5 +1,7 @@
 package com.cursos.api.authorizationserver.config.security;
 
+import com.cursos.api.authorizationserver.persistence.repository.UserRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
